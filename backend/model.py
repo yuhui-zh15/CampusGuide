4f936b16d2837f950c76c5d901040bf4faf971f7
Paula Zhou <zhouzp15@mails.tsinghua.edu.cn>
2018-05-26 14:04:54 +0800

    fix?

--- backend/model.py
+++ backend/model.py
@@ -11,43 +11,43 @@ from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Flatten
 
 def model_fn(config):
     model = Sequential()
-    model.add(Convolution2D(
+    model.add(Convolution2D(                        # 0
         batch_input_shape=(None, 640, 360, 3),
         filters=32,
         kernel_size=5,
         strides=1,
         padding='same'     # Padding method
     ))
-    model.add(Activation('relu'))
+    model.add(Activation('relu'))                   # 1
 
-    model.add(MaxPooling2D(
+    model.add(MaxPooling2D(                         # 2
         pool_size=4,
         strides=4,
         padding='same'    # Padding method
     ))
 
     # 160 x 90 x 32
-    model.add(Convolution2D(
+    model.add(Convolution2D(                        # 3
         batch_input_shape=(None, 160, 90, 32),
         filters=64,
         kernel_size=5,
         strides=1,
         padding='same'     # Padding method
     ))
-    model.add(Activation('relu'))
+    model.add(Activation('relu'))                   # 4
 
-    model.add(MaxPooling2D(
+    model.add(MaxPooling2D(                         # 5
         pool_size=5,
         strides=5,
         padding='same'    # Padding method
     ))
 
     # 32 x 18 x 64
-    model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Activation('relu'))
-    model.add(Dense(config.num_of_class))
-    model.add(Activation('softmax'))
+    model.add(Flatten())                            # 6
+    model.add(Dense(1024))                          # 7
+    model.add(Activation('relu'))                   # 8
+    model.add(Dense(config.num_of_class))           # 9
+    model.add(Activation('softmax'))                # 10
 
     adam = Adam(lr=config.lr)
     model.compile(optimizer=adam,
--- backend/webapi/model_wrapper.py
+++ backend/webapi/model_wrapper.py
@@ -1,5 +1,6 @@
 #encoding=utf-8
 import numpy as np
+import cv2
 from PIL import Image
 import tensorflow as tf
 from keras import backend as K
@@ -21,40 +22,70 @@ class ModelWrapper(object):
     
     @staticmethod
     def normalize(img):
-        img = img[:, :, ::-1]   # BGR -> RGB
-        img = Image.fromarray(img)
-        std_width, std_height = 360, 640
-        width, height = img.size
-        scale_x = std_width / float(width)
-        scale_y = std_height / float(height)
-        scale = max(scale_x, scale_y)
-        img = img.resize((int(width * scale + 0.5), int(height * scale + 0.5)), 
-                Image.ANTIALIAS)
-        width, height = img.size
-        left = (width - std_width) / 2
-        top = (height - std_height) / 2
-        right = (width + std_width) / 2
-        bottom = (height + std_height) / 2
-        img = img.crop((left, top, right, bottom))
-        img.save('a.png')
-        X = np.array(img)
-        return np.array([X - 128.0 / 255.0])
+        # img = cv2.resize(img, (360, 640))
+        # img = Image.fromarray(img)
+        # std_width, std_height = 360, 640
+        # width, height = img.size
+        # scale_x = std_width / float(width)
+        # scale_y = std_height / float(height)
+        # scale = max(scale_x, scale_y)
+        # img = img.resize((int(width * scale + 0.5), int(height * scale + 0.5)), 
+        #         Image.ANTIALIAS)
+        # width, height = img.size
+        # left = (width - std_width) / 2
+        # top = (height - std_height) / 2
+        # right = (width + std_width) / 2
+        # bottom = (height + std_height) / 2
+        # img = img.crop((left, top, right, bottom))
+        # img.save('a.png')
+        # X = np.array(img)
+        # return np.array([img - 128.0 / 255.0])
+        return np.array([(cv2.resize(img, (360, 640)) - 128.0) / 255.0])
 
 
     @classmethod
     def predict(cls, img):
         with cls.graph.as_default():
             X = cls.normalize(img)
-            y = cls.model.predict(X)
 
             #*******************DEBUG*********************
-            inputs = cls.model.input
-            outputs = [layer.output for layer in cls.model.layers]
-            functors = [K.function([inputs], [out]) for out in outputs]
-            layer_outs = [func([X]) for func in functors]
-            print layer_outs
+            def print_layer(layer_idx):
+                inputs_op = cls.model.input
+                print cls.model.layers[layer_idx].name
+                output_op = cls.model.layers[layer_idx].output
+                output_fn = K.function([inputs_op], [output_op])
+                output = output_fn([X])[0][0]
+                print output.shape
+
+                if output.ndim == 3:    # CNN layer
+                    for channel_idx in xrange(output.shape[-1]):
+                        channel = output[:, :, channel_idx]
+                        mi, mx = np.min(channel), np.max(channel)
+                        if mi == mx:
+                            print 'layer:', layer_idx, 'channel:', channel_idx, 'value:', mx
+                            continue
+                        else:
+                            print 'layer:', layer_idx, 'channel:', channel_idx, 'max:', mx, 'min:', mi, 'mean:', np.mean(channel)
+                        channel = ((channel - mi) / float(mx - mi)) * 255
+                        cv2.imwrite('.debug/%d-%d.png' % (layer_idx, channel_idx), channel)
+                else:
+                    print output
+                print
+
+            # print_layer(1)
+            # print_layer(4)
+            # print_layer(9)
+            # print_layer(10)
             #*******************DEBUG*********************
 
-            predicted = np.argmax(y, axis=1)[0]
-            building = id2building[predicted]
-            return building
+            return id2building[np.argmax(cls.model.predict(X)[0])]
+
+
+if __name__ == '__main__':
+    # img_name = '../images/test/1_qingfen_mini/qingfen_1.png'
+    img_name = '.debug/input.jpg'
+    img = cv2.imread(img_name)
+    print img.dtype
+    img = np.array([(cv2.resize(img, (360, 640)) - 128.0) / 255.0])
+    print id2building[np.argmax(ModelWrapper.model.predict(img)[0])]
+
