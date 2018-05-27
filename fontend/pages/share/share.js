8d68dd910036c219741432836e4e6521be85fcf7
周正平 <zhouzp15@mails.tsinghua.edu.cn>
2018-05-27 19:03:06 +0800

    rm redundant console.log

--- fontend/pages/share/share.js
+++ fontend/pages/share/share.js
@@ -26,7 +26,6 @@ Page({
     var that = this
     var openid = wx.getStorageSync('openid')
     var option = 1
-    console.log("http://166.111.5.246:8080/share?" + "openid=" + openid + "&option=" + option)
     wx.downloadFile({
       url: "http://166.111.5.246:8080/share?" + "openid=" + openid + "&option=" + option,
       success: function (res) {
