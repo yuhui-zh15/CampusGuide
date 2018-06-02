619b048960105741faa3eb5dff93258b64814be8
周正平 <zhouzp15@mails.tsinghua.edu.cn>
2018-06-02 22:11:20 +0800

    frontend ok! (add navigation, meet listview)

--- fontend/pages/scene/scene.js
+++ fontend/pages/scene/scene.js
@@ -1,71 +1,27 @@
-// pages/scene/scene.js
+// pages/image/demo3/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    predict1: "水木清华",
-    predict2: "第六教学楼",
-    predict3: "第三教学楼",
-    photo1: "../../images/demo/shuimuqinghua.jpg",
-    photo2: "../../images/demo/diliujiaoxuelou.jpg",
-    photo3: "../../images/demo/disanjiaoxuelou.jpg"
+  data:{
+    imgList:[
+      { src: 'http://d.ifengimg.com/w600/p0.ifengimg.com/pmop/2017/1130/08D143AB45D03F261611129AD6C0CB580CA13A15_size165_w1107_h776.jpeg', info: '人文社科图书馆' },
+      { src: 'http://kaoyan.eol.cn/nnews/201706/W020170622324420019340.jpg', info: '第三教学楼' },
+      { src: 'http://www.sinaimg.cn/dy/slidenews/11_img/2009_23/264_32_278332.jpg', info: '第六教学楼' },
+      { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/%E6%B8%85%E5%8D%8E%E5%9B%AD%E5%B9%BF%E5%9C%BA.JPG/1200px-%E6%B8%85%E5%8D%8E%E5%9B%AD%E5%B9%BF%E5%9C%BA.JPG', info: '大礼堂' },
+      { src: 'https://imgs.qunarzz.com/sight/p0/1607/ce/ce94534274b3bd6db4.water.jpg_750x500_b362cec7.jpg', info: '二校门' },
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  onReady:function(){
+    // 页面渲染完成
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  onShow:function(){
+    // 页面显示
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  onHide:function(){
+    // 页面隐藏
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onUnload:function(){
+    // 页面关闭
   }
 })
